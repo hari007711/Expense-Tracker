66757c358c4c396577e6b8aad5bb8998306dffdb
Hari <hari0077kumar@gmail.com>
2025-08-01 15:02:56 +0530

    css

--- budgetApp/src/components/Expense/AddExpense.tsx
+++ budgetApp/src/components/Expense/AddExpense.tsx
@@ -116,7 +116,7 @@ export default function AddExpense() {
           </DialogTrigger>
         </div>
 
-        <DialogContent className="w-[30vw]">
+        <DialogContent className="w-[30vw] min-w-[30vw]">
           <form onSubmit={handleSubmit(onSubmit)}>
             <DialogHeader>
               <DialogTitle>Add Expense</DialogTitle>
